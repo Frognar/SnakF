dfed00cdce02f6b6c56732d9e47e4f915e01b423
frognar <sebastian.przyszlak@gmail.com>
2024-12-25 20:05:29 +0100

    can end in start when turn 4 times left or right

--- tests/SnakeF.Tests/SnakeTests.fs
+++ tests/SnakeF.Tests/SnakeTests.fs
@@ -22,3 +22,39 @@ let ``snake cannot turn 180 degrees`` () =
         move startingPosition direction newDirection = move startingPosition direction direction
 
     Check.QuickThrowOnFailure cannotTurn180
+
+[<Fact>]
+let ``can end in to starting position when turn right 4 times`` () =
+    let nextRight (direction: direction) =
+        match direction with
+        | North -> East
+        | South -> West
+        | East -> South
+        | West -> North
+
+    let canGoBack (startingPosition: position) (initialDirection: direction) =
+        let rec loop i pos prevDir dir =
+            if i = 4 then pos
+            else loop (i + 1) (move pos prevDir dir) dir (nextRight dir)
+        let endingPosition = loop 0 startingPosition initialDirection initialDirection
+        endingPosition = startingPosition
+
+    Check.QuickThrowOnFailure canGoBack
+
+[<Fact>]
+let ``can end in starting position when turn left 4 times`` () =
+    let nextLeft (direction: direction) =
+        match direction with
+        | North -> West
+        | South -> East
+        | East -> North
+        | West -> South
+
+    let canGoBack (startingPosition: position) (initialDirection: direction) =
+        let rec loop i pos prevDir dir =
+            if i = 4 then pos
+            else loop (i + 1) (move pos prevDir dir) dir (nextLeft dir)
+        let endingPosition = loop 0 startingPosition initialDirection initialDirection
+        endingPosition = startingPosition
+
+    Check.QuickThrowOnFailure canGoBack
