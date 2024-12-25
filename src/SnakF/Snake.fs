a1788251a1dfbeaa6c0a4812c11a2fef55f4967e
frognar <sebastian.przyszlak@gmail.com>
2024-12-25 19:54:46 +0100

    snake cannot turn 180 degrees

--- src/SnakF/Snake.fs
+++ src/SnakF/Snake.fs
@@ -4,6 +4,22 @@ type direction = North | South | East | West
 
 type position = int * int
 
-let move (startingPosition: position) (direction: direction) : position =
+let is180Turn (previousDirection: direction) (direction: direction) =
+    match previousDirection, direction with
+    | North, South
+    | South, North
+    | East, West
+    | West, East -> true
+    | _ -> false
+
+let move (startingPosition: position) (previousDirection: direction) (direction: direction) : position =
     let x, y = startingPosition
-    (x, y + 1)
+    match
+        if is180Turn previousDirection direction
+        then previousDirection
+        else direction
+    with
+    | North -> x, y - 1
+    | South -> x, y + 1
+    | East -> x + 1, y
+    | West -> x - 1, y
--- tests/SnakeF.Tests/SnakeTests.fs
+++ tests/SnakeF.Tests/SnakeTests.fs
@@ -7,5 +7,18 @@ open FsCheck
 [<Fact>]
 let ``after move should be in different position`` () =
     let rec moveToDifferentPosition (startingPosition: position) (direction: direction) =
-        move startingPosition direction <> startingPosition
+        move startingPosition direction direction <> startingPosition
+
     Check.QuickThrowOnFailure moveToDifferentPosition
+
+[<Fact>]
+let ``snake cannot turn 180 degrees`` () =
+    let rec cannotTurn180 (startingPosition: position) (direction: direction) =
+        let newDirection = match direction with
+                            | North -> South
+                            | South -> North
+                            | East -> West
+                            | West -> East
+        move startingPosition direction newDirection = move startingPosition direction direction
+
+    Check.QuickThrowOnFailure cannotTurn180
